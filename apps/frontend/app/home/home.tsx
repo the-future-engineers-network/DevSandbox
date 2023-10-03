91a2b0fca2bb5b49d6cd5960f4927f25ece9de4d
Javascript Enthusiast <abmercy035@gmail.com>
2023-10-03 09:28:11 +0100

    Design:
    completed the No Setup section, but the Video refused to display tried import statement but got an error.

--- apps/frontend/app/home/home.tsx
+++ apps/frontend/app/home/home.tsx
@@ -3,6 +3,13 @@ import Link from "next/link";
 import { ShareIcon, ChevronRight } from "../icons";
 import styles from "./home.module.css";
 
+{
+    /*CHENEDU I GET  THIS ERROR WHILE TRYING TO USE THE IMPORT FILE METHOD
+* <You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders> (Source code omitted for this binary file)
+*/}
+// import no_setup_video from "./assets/nosetup.mp4"
+
+
 export function Home(): JSX.Element {
   return (
     <>
@@ -49,10 +56,20 @@ export function Home(): JSX.Element {
             No setup. <span>One click and start coding.</span>
           </h2>
           <p>
-            Start coding instantly without worrying about the configuration. Our
-            sandboxes have everything you need: dev servers, unit tests,
-            Storybook and other devtools.
+            Start coding instantly without worrying about the configuration.
+            <span> Our sandboxes have everything you need: dev servers, unit tests, Storybook and other devtools.</span>
           </p>
+          <div>
+
+            <video width="100%" height="100%" muted>
+              {/* <source src="" type="video/webm" /> */}
+              {/* Video Not playing */}
+              <source src="./assets/nosetup.mp4" type="video/mp4" />
+              {/*  Couldnt use it cause of the error*/}
+              {/* <source src={no_setup_video} type="video/mp4" /> */}
+            </video>
+
+          </div>
         </section>
 
         <section className={styles.shareableUrls}>
