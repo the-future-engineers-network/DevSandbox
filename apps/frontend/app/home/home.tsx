38a9d546741141fe5572f53a3fb4903ba6c96832
Abraham Samuel {JavaScript Enthusiast} <63074094+abmercy035@users.noreply.github.com>
2023-10-10 07:22:06 +0000

    Code clean Up:
    Clean up and removed unwanted codes and comment

--- apps/frontend/app/home/home.tsx
+++ apps/frontend/app/home/home.tsx
@@ -3,10 +3,6 @@ import Link from "next/link";
 import { ShareIcon } from "../icons";
 import styles from "./home.module.css";
 
-{
-    /*CHENEDU I GET  THIS ERROR WHILE TRYING TO USE THE IMPORT FILE METHOD
-* <You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders> (Source code omitted for this binary file)
-*/}
 
 
 export function Home(): JSX.Element {
@@ -26,17 +22,16 @@ export function Home(): JSX.Element {
 
       <main>
         <section className={styles.noSetup}>
-          {/* TODO: Add no setup content here. The first bit of content has been set up for you. */}
           <div className={styles.info}>
             <h2>
               No setup. <span>One click and start coding.</span>
             </h2>
             <p>
               Start coding instantly without worrying about the configuration.
-              <span> Our sandboxes have everything you need: dev servers, unit tests, Storybook and other devtools.</span>
+              <span className={styles.infoNote}> Our sandboxes have everything you need: dev servers, unit tests, Storybook and other devtools.</span>
             </p>
           </div>
-          <div className={styles.noSetup_video}>
+          <div className={styles.noSetupVideo}>
             <video width="100%" height="100%" muted autoPlay loop>
               <source src="/nosetup.mp4" type="video/mp4" />
             </video>
