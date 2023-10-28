13f21c608585e984fcad3ff93f88ece612585ce0
Chinedu Daniel <chinedudaniel7@gmail.com>
2023-10-28 13:46:37 +0200

    Cleanup shareable url section

--- apps/frontend/app/home/home.tsx
+++ apps/frontend/app/home/home.tsx
@@ -4,80 +4,59 @@ import Link from "next/link";
 import { ArrowIcon, ClickIcon, GroupIcon, LinkIcon, ShareIcon } from "../icons";
 import styles from "./home.module.css";
 
-export function Home(): JSX.Element {
-  const sharableImages = ["/shareurl.webp", "/context.webp", "/comment.webp"];
-  const [currentImage, setCurrentImage] = useState({
-    className: "trim",
-    src: sharableImages[0],
-  });
-  const [progressBars, setProgressBars] = useState({
-    type: "progressBar1",
-    value: 0,
-  });
-  const progressBarTypes = ["progressBar1", "progressBar2", "progressBar3"];
-  function handleProgressBars(): void {
-    const { type, value } = progressBars;
-    if (value < 100) {
-      setProgressBars({
-        type: type,
-        value: value + 0.5,
-      });
-    } else {
-      let nextType: string;
-      if (type === "progressBar1") {
-        nextType = "progressBar2";
-      } else if (type === "progressBar2") {
-        nextType = "progressBar3";
-      } else {
-        nextType = "progressBar1";
-      }
-      setProgressBars({
-        type: nextType,
-        value: 0,
-      });
-    }
-  }
+const shareableMessages = [
+  {
+    title: "Share a link",
+    description:
+      "Forget sharing screenshots and copy-pasting code snippets. Get instant feedback on your code by sharing the URL of your sandbox with anyone.",
+    imageUrl: "/shareurl.webp",
+    icon: <LinkIcon height={27} width={27} />,
+  },
+  {
+    title: "Share the context",
+    description:
+      "Those opening your sandbox will see your code running along with any tests, Storybook, or tasks you configured. No need to set up anything.",
+    imageUrl: "/context.webp",
+    icon: <ClickIcon height={27} width={27} />,
+  },
+  {
+    title: "Powerful collaboration",
+    description:
+      "Get async feedback through comments or host live coding sessions to learn together and guide others through the code.",
+    imageUrl: "/comment.webp",
+    icon: <GroupIcon height={27} width={27} />,
+  },
+];
 
-  function handleImageSwitch(id: number): void {
-    setCurrentImage({
-      ...currentImage,
-      className: "trim",
-    });
-    setTimeout(() => {
-      setCurrentImage({
-        src: sharableImages[id - 1],
-        className: "animatedContainer",
-      });
-    }, 300);
-  }
+export function Home(): JSX.Element {
+  const [activeIdx, setActiveIdx] = useState(0);
+  const [progress, setProgress] = useState(0);
+  const activeImage = shareableMessages[activeIdx];
+
+  const updateProgress = (): void => {
+    setProgress((previousProgress) => (previousProgress + 0.5) % 100);
+  };
+
+  const handleMessageInteraction = (idx: number) => () => {
+    setActiveIdx(idx);
+    setProgress(0);
+  };
 
   useEffect(() => {
-    const id = setInterval(handleProgressBars, 35);
+    const id = setInterval(updateProgress, 35);
+
     return () => {
       clearInterval(id);
     };
-  });
+  }, []);
 
   useEffect(() => {
-    for (let i = 0; i <= progressBarTypes.length - 1; i++) {
-      if (progressBars.type === progressBarTypes[i]) {
-        handleImageSwitch(i + 1);
-      }
+    if (progress >= 99) {
+      setActiveIdx((previousIdx) => (previousIdx + 1) % 3);
+      setProgress(0);
     }
-  }, [progressBars.type]);
+  }, [progress]);
 
-  const activeStyle = {
-    width: `${progressBars.value}%`,
-    className: "active",
-  };
-  const inactiveStyle = {
-    width: "0%",
-    className: "inactive",
-  };
-  // To handle MouseOvers
-  const handleProgressBar = (id: number): void => {
-    setProgressBars({ value: 0, type: progressBarTypes[id - 1] });
-  };
   return (
     <>
       <header className={styles.header}>
@@ -111,107 +90,47 @@ export function Home(): JSX.Element {
             Get instant <br /> feedback, the <br /> easy way.
           </h2>
           <div className={styles.slideContainer}>
-            <div className={styles[currentImage.className]}>
-              <img alt="share a link" src={currentImage.src} />
+            <div className={styles.imageContainer}>
+              <Image
+                alt="todo: add proper alt text"
+                height={600}
+                sizes="100vw"
+                src={activeImage.imageUrl}
+                style={{
+                  width: "100%",
+                  height: "auto",
+                }}
+                width={1080}
+              />
             </div>
+
             <div className={styles.slider}>
-              <div
-                className={
-                  progressBars.type === progressBarTypes[0]
-                    ? styles[activeStyle.className]
-                    : "inactive"
-                }
-                onFocus={() => {
-                  handleProgressBar(1);
-                }}
-                onMouseOver={() => {
-                  handleProgressBar(1);
-                }}
-              >
-                <div className={styles.progressBarContainer}>
+              {shareableMessages.map(({ title, description, icon }, idx) => {
+                const isActive = idx === activeIdx;
+                return (
                   <div
-                    style={
-                      progressBars.type === progressBarTypes[0]
-                        ? activeStyle
-                        : inactiveStyle
-                    }
-                  />
-                </div>
-                <LinkIcon height={27} width={27} />
-                <div className={styles.shareALinkContent}>
-                  <h2>Share a link</h2>
-                  <p>
-                    Forget sharing screenshots and copy-pasting code snippets.
-                    Get instant feedback on your code by sharing the URL of your
-                    sandbox with anyone.
-                  </p>
-                </div>
-              </div>
-              <div
-                className={
-                  progressBars.type === progressBarTypes[1]
-                    ? styles[activeStyle.className]
-                    : "inactive"
-                }
-                onFocus={() => {
-                  handleProgressBar(2);
-                }}
-                onMouseOver={() => {
-                  handleProgressBar(2);
-                }}
-              >
-                <div className={styles.progressBarContainer}>
-                  <div
-                    className={styles.pop}
-                    style={
-                      progressBars.type === progressBarTypes[1]
-                        ? activeStyle
-                        : inactiveStyle
-                    }
-                  />
-                </div>
-                <ClickIcon height={27} width={27} />
-                <div className={styles.shareALinkContent}>
-                  <h2>Share the context</h2>
-                  <p>
-                    Those opening your sandbox will see your code running along
-                    with any tests, Storybook, or tasks you configured. No need
-                    to set up anything.
-                  </p>
-                </div>
-              </div>
-              <div
-                className={
-                  progressBars.type === progressBarTypes[2]
-                    ? styles[activeStyle.className]
-                    : "inactive"
-                }
-                onFocus={() => {
-                  handleProgressBar(3);
-                }}
-                onMouseOver={() => {
-                  handleProgressBar(3);
-                }}
-              >
-                <div className={styles.progressBarContainer}>
-                  <div
-                    style={
-                      progressBars.type === progressBarTypes[2]
-                        ? activeStyle
-                        : inactiveStyle
-                    }
-                  />
-                </div>
-                <GroupIcon height={27} width={27} />
-                <div className={styles.shareALinkContent}>
-                  <h2>Powerful collaboration</h2>
-                  <p>
-                    Get async feedback through comments or host live coding
-                    sessions to learn together and guide others through the
-                    code.
-                  </p>
-                </div>
-              </div>
+                    key={title}
+                    onFocus={handleMessageInteraction(idx)}
+                    onMouseOver={handleMessageInteraction(idx)}
+                  >
+                    <div className={styles.progressBarContainer}>
+                      <div
+                        style={{
+                          width: isActive ? `${progress}%` : "0%",
+                          height: "100%",
+                          backgroundColor: "var(--primary)",
+                        }}
+                      />
+                    </div>
+                    {icon}
+                    <div className={styles.shareALinkContent}>
+                      <h2>{title}</h2>
+                      <p>{description}</p>
+                    </div>
+                  </div>
+                );
+              })}
+
               <Link className={styles.getStarted} href="#">
                 <h3>
                   Get Started <ArrowIcon />
