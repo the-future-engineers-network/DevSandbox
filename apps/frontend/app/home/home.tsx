226f30e9d4fc1d6a96448f920869bd838b608a3c
sophrone <46854009+sophrone@users.noreply.github.com>
2023-11-18 13:54:45 +0100

    Feature 17 (#36)
    * Added new code to files
    
    * Removed an added line from globals.css
    
    * Add the header section of the homepage
    
    * Installed prettier formatter and updated home.module.css file
    
    * updated home.module.css file
    
    * Formatted chevron-right.tsx and index.tsx files manually

--- apps/frontend/app/home/home.tsx
+++ apps/frontend/app/home/home.tsx
@@ -1,21 +1,45 @@
 import Image from "next/image";
 import Link from "next/link";
-import { ShareIcon } from "../icons";
+import { ShareIcon, ChevronRight } from "../icons";
 import styles from "./home.module.css";
 
 export function Home(): JSX.Element {
   return (
     <>
       <header className={styles.header}>
-        {/* TODO: Add header content here. The first bit of content has been set up for you.  */}
-        <nav>
-          <Link href="/">
-            <Image alt="logo" height={24} src="/logo.svg" width={24} />
-          </Link>
-
-          <Link href="#">Sign in</Link>
-          <Link href="#">Try for free</Link>
+        <nav className={styles.mainNav}>
+          <div className={styles.navOne}>
+            <Link href="/">
+              <Image alt="logo" height={24} src="/logo.svg" width={24} />
+            </Link>
+            <Link href="#">DevSandbox</Link>
+          </div>
+          <div className={styles.navTwo}>
+            <Link href="#">Sign in</Link>
+            <Link href="#">
+              <button type="submit">Try for free</button>
+            </Link>
+          </div>
         </nav>
+        <div className={styles.largeHeader}>
+          <h1>Code in</h1>
+          <h1>Sandboxes</h1>
+        </div>
+        <div className={styles.smallHeader}>
+          <p>Prototype your ideas with shareable</p>
+          <p>and secure cloud sandboxes.</p>
+        </div>
+        <div className={styles.navThree}>
+          <Link href="#">Create sandbox </Link>
+          <Link href="#">
+            <ChevronRight />
+          </Link>
+        </div>
+        <div className={styles.videoContainer}>
+          <video width="90%" height="100%" autoPlay loop muted>
+            <source src="/video.mp4" type="video/mp4" />
+          </video>
+        </div>
       </header>
 
       <main>
